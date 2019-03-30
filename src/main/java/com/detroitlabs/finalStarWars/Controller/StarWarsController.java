4631c65b4478eae85d5c02e23dff1db7fb7c02a1
NM17 <nada.mahmoud@detroitlabs.com>
2019-03-30 18:58:03 -0400

    inital commit

---
+++ src/main/java/com/detroitlabs/finalStarWars/Controller/StarWarsController.java
@@ -0,0 +1,8 @@
+package com.detroitlabs.finalStarWars.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StarWarsController {
+
+}
---
+++ src/main/java/com/detroitlabs/finalStarWars/Services/StarWarsService.java
@@ -0,0 +1,7 @@
+package com.detroitlabs.finalStarWars.Services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StarWarsService {
+}
