e1fab7975cd2e26ea6d33c2768467fbebddcd897
NM17 <nada.mahmoud@detroitlabs.com>
2019-03-30 19:09:13 -0400

    adds a home html page

--- src/main/java/com/detroitlabs/finalStarWars/Services/StarWarsService.java
+++ src/main/java/com/detroitlabs/finalStarWars/Services/StarWarsService.java
@@ -1,7 +1,14 @@
 package com.detroitlabs.finalStarWars.Services;
 
+import com.detroitlabs.finalStarWars.Model.AllStarWarsData;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class StarWarsService {
+
+    public AllStarWarsData fetchStarWarsInfo(){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("https://swapi.co/api/films/", AllStarWarsData.class);
+    }
 }
